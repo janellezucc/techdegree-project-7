592db59ff76cd8afa21b517eeabe8c408a53a9dd
Janelle <janellezucc@gmail.com>
2019-12-04 08:47:23 -0500

    alert banner close

--- app.js
+++ app.js
@@ -6,5 +6,11 @@ alertBanner.innerHTML =
 <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
 to complete</p>
 <p class="alert-banner-close">x</p>
-</div>
-`
+</div> `
+
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+    alert.style.display = "none"
+    }
+});
