08319a413b6a556639fc95c47c0ce23db850dd30
Janelle <janellezucc@gmail.com>
2019-12-05 08:44:20 -0500

    daily traffic widget

--- app.js
+++ app.js
@@ -62,3 +62,36 @@ let trafficChart = new Chart(trafficCanvas, {
     data: trafficData,
     options: trafficOptions
 });
+
+
+// Bar Graph //
+const dailyCanvas = document.getElementById("daily-chart");
+
+// data for daily traffic bar chart
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+    label: '# of Hits',
+    data: [75, 115, 175, 125, 225, 200, 100],
+    backgroundColor: '#7477BF',
+    borderWidth: 1
+    }]
+    };
+    const dailyOptions = {
+    scales: {
+    yAxes: [{
+    ticks: {
+    beginAtZero:true
+    }
+    }]
+    },
+    legend : {
+    display: false
+    }
+    }
+
+    let dailyChart = new Chart(dailyCanvas, {
+        type: 'bar',
+        data: dailyData,
+        options: dailyOptions
+        });
