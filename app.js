2a0f5abc3d522f0f1901aac13727c99202a65dbe
Janelle <janellezucc@gmail.com>
2019-12-06 10:47:57 -0500

    doughnut chart

--- app.js
+++ app.js
@@ -100,3 +100,37 @@ const dailyData = {
         data: dailyData,
         options: dailyOptions
 });
+
+// Doughnut Chart //
+
+const mobileCanvas = document.getElementById("mobile-users-chart");
+
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+    label: '# of Users',
+    data: [2000, 550, 500],
+    borderWidth: 0,
+    backgroundColor: [
+    '#7477BF',
+    '#78CF82',
+    '#51B6C8'
+    ]
+    }]
+};
+
+const mobileOptions = {
+    legend: {
+    position: 'right',
+    labels: {
+    boxWidth: 20,
+    fontStyle: 'bold'
+    }
+    }
+}
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
+ });
